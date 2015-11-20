64628143191ab242c38903b5594bb35f93bcc1db
Tref <nick@treflabs.com>
2015-11-20 13:28:04 -0500

    moving array extension to global js file

--- battleship-ember/ember-cli-build.js
+++ battleship-ember/ember-cli-build.js
@@ -5,10 +5,10 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
   });
-
   app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.js');
   app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
   app.import(app.bowerDirectory + '/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2', {destDir: 'fonts'});
+  app.import('vendor/js/array-extension.js');
     
   // Use `app.import` to add additional libraries to the generated
   // output files.
